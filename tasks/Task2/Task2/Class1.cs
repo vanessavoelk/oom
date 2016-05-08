0bea0a7ee30c7a03d6c08124bc2a6d259cb13bd8
unknown <vanessavoelk>
2016-05-08 22:31:36 +0200

    add project Task2

---
+++ tasks/Task2/Task2/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class Computer
+    {
+        private decimal m_preis;
+        private int m_anzahl=0;
+        
+
+        public decimal Preis
+        {
+            get //Ausgeführt wenn Wert gelesen wird
+            {
+                return m_preis;
+            }
+            set
+            {
+                if (value < 0) throw new Exception("Preis kann nicht negativ sein!");
+                m_preis = value;
+            }
+        }
+
+        public int Anzahl
+        {
+            get
+            {
+                return m_anzahl;
+            }
+            set
+            {
+                if (value < 0) throw new Exception("Anzahl kann nicht negativ sein!");
+                m_anzahl = value;
+            }
+        }
+
+        public decimal Endpreis()
+        {
+            return (m_anzahl * m_preis);
+        }
+
+        public Computer(decimal newm_preis, int newm_anzahl)
+        {
+            if (newm_preis < 0) throw new ArgumentOutOfRangeException("Preis kann nicht negativ werden!");
+            if (newm_anzahl < 0) throw new ArgumentOutOfRangeException("Anzahl kann nicht negativ werden!");
+            m_preis = newm_preis;
+            m_anzahl = newm_anzahl; //warum bleibt der Lagerstand 1??
+        }
+    }
+}
---
+++ tasks/Task2/Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer a = new Computer(499, 1);
+            Computer b = new Computer(599, 4);
+            Computer c = new Computer(699, 2);
+            Computer d = new Computer(799, 3);
+
+            Console.WriteLine($"Der Preis von Computer a  ist {a.Preis} EURO, ihn gibt es {a.Anzahl} mal und der Gesamtpreis dafür ist {a.Endpreis()} ");
+            Console.WriteLine($"Der Preis von Computer b  ist {b.Preis} EURO, ihn gibt es {b.Anzahl} mal und der Gesamtpreis dafür ist {b.Endpreis()} ");
+            Console.WriteLine($"Der Preis von Computer c  ist {c.Preis} EURO, ihn gibt es {c.Anzahl} mal und der Gesamtpreis dafür ist {c.Endpreis()} ");
+            Console.WriteLine($"Der Preis von Computer d  ist {d.Preis} EURO, ihn gibt es {d.Anzahl} mal und der Gesamtpreis dafür ist {d.Endpreis()} ");
+
+        }
+    }
+}
